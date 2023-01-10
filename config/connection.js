75fe7dee4eb5df22a46fe78a0df6c75e1b7d8ee1
Andrew Leonard <AndrewCLeonard@fastmail.com>
2023-01-09 20:14:38 -0800

    added /config

--- config/connection.js
+++ config/connection.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+mongoose.set("strictQuery", false);
 
 main().catch((err) => console.log(err));
 
@@ -6,4 +7,36 @@ async function main() {
 	await mongoose.connect("mongodb://127.0.0.1:27017/test");
 
 	// use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+
+	const kittySchema = new mongoose.Schema({
+		name: String,
+	});
+
+	// NOTE: methods must be added to the schema before compiling it with mongoose.model()
+	kittySchema.methods.speak = function speak() {
+		const greeting = this.name ? "Meow name is " + this.name : "I don't have a name";
+		console.log(greeting);
+	};
+
+	const Kitten = mongoose.model("Kitten", kittySchema);
+
+	const silence = new Kitten({ name: "Silence" });
+	// console.log(silence.name); // 'Silence'
+
+	const fluffy = new Kitten({ name: "fluffy" });
+	// fluffy.speak(); // "Meow name is fluffy"
+
+	const Jared = new Kitten({ breed: "Jared" });
+
+	await fluffy.save();
+	fluffy.speak();
+	silence.speak();
+	Jared.speak();
+
+	const kittens = await Kitten.find();
+	// console.log(kittens);
+
+	// We just logged all of the kittens in our db to the console. If we want to filter our kittens by name, Mongoose supports MongoDBs rich querying syntax.
+
+	// await Kitten.find({ name: /^fluff/ });
 }
